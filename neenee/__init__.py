f059d9bb613df92ab86a2ddf6685d7699cf40aea
HitBlast <hitblastlive@gmail.com>
2024-05-04 13:53:06 +0600

    modified cli structure to not run cli directly from script

--- neenee/__init__.py
+++ neenee/__init__.py
@@ -1,1 +1,5 @@
-from .bot import *
+from .bot import (
+    Neenee as Neenee, 
+    build_core as build_core
+)
+from .cli import cli as cli
--- neenee/main.py
+++ neenee/main.py
@@ -1,43 +1,4 @@
-# Imports.
-import click
-from decouple import config
-from rich.console import Console
+from neenee import cli
 
-
-# Create a new group for putting the CLI commands.
-@click.group(help="A personal Discord bot (especially for developers), acting as both a robust testbed and a fun bot :3")
-@click.version_option(
-    package_name="Neenee",
-    message="Package: %(prog)s, version %(version)s\n",
-)
-def cli() -> None:
-    pass
-
-
-# Initialize Console object for rich-based output.
-console = Console()
-
-
-# Helper functions for CLI commands.
-def _print_err(text: str) -> None:
-    console.print(text, style="red")
-    return click.echo(err=True)
-
-
-# The main function for running the bot.
-@cli.command("run", help="Run the bot.")
-def _run() -> None:
-    # Create a new instance of Neenee.
-    from neenee import build_core
-    neenee = build_core()
-
-    try:
-        # Run the bot.
-        neenee.run(config("DISCORD_TOKEN", cast=str))
-    except Exception as e:
-        _print_err(f"error -_- {e}")
-
-
-# Run the main function.
 def main() -> None:
     cli()
